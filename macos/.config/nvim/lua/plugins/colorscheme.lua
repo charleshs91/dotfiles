ddfd88cdaea776c7d12cd64df767ff1a7a0d4b37
Charles Hsieh <charleshs91tw@gmail.com>
2025-12-11 14:14:53 +0800

    nvim: update configs

--- macos/.config/nvim/lua/plugins/colorscheme.lua
+++ macos/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,33 +1,36 @@
 return {
-
-  -- set colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
-
-  -- install and configure gruvbox: https://github.com/ellisonleao/gruvbox.nvim
+  -- https://github.com/ellisonleao/gruvbox.nvim
   {
     "ellisonleao/gruvbox.nvim",
     lazy = false,
     priority = 1000,
     opts = {
       transparent_mode = true,
+      contrast = "hard",
     },
   },
 
-  -- -- install and configure tokyonight
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   priority = 1000,
-  --   opts = {
-  --     transparent = true,
-  --     styles = {
-  --       sidebars = "normal",
-  --       floats = "normal",
-  --     },
-  --   },
-  -- },
+  -- https://github.com/folke/tokyonight.nvim
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      style = "night",
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
+  -- set colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+      -- colorscheme = "tokyonight",
+    },
+  },
 }
--- macos/.config/nvim/lua/plugins/extend-lspconfig.lua
+++ macos/.config/nvim/lua/plugins/extend-lspconfig.lua
@@ -2,6 +2,9 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
+      bashls = {
+        filetypes = { "sh", "zsh" },
+      },
       sourcekit = {
         cmd = { "xcrun", "sourcekit-lsp" },
       },
