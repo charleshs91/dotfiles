4c212b4f8020539c8c9e47eeee32a7a76f454fb4
Charles Hsieh <charleshs91tw@gmail.com>
2026-01-04 03:27:28 +0800

    zsh: add aliases

--- macos/.config/zsh/aliases.sh
+++ macos/.config/zsh/aliases.sh
@@ -2,8 +2,11 @@ alias c=clear
 alias code=codium
 alias gdu=gdu-go
 alias lg=lazygit
-alias v="fd --type f --hidden --exclude .git | fzf-tmux -p --reverse | xargs nvim"
+alias v=vim
 alias vim=nvim
+
+FZF_OPTS="--height 40% --reverse --preview 'bat --color=always {}' --preview-window '~3'"
+alias vv="fd --type f --hidden --exclude .git | fzf $FZF_OPTS | xargs nvim"
 
 # git
 alias gs="git status --short --branch"
